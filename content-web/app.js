d10484b38fe914e03c29e91ef0a2e0185837bd51
SMSeerat <smseerat11@gmail.com>
2022-04-07 09:48:57 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("bafd15c9-6e09-41bf-8ee3-1b2bda201ac3");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
